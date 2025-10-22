7cd4a052d0201442ebabb7af27271906380dc2b1
Em_25 <emmathehunter2007@gmail.com>
2025-10-21 23:54:55 -0400

    Task 2

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,2 +1,19 @@
-public class FavNumbers {
+import java.util.Scanner;
+
+public class FavNumbers
+{
+    public static void main(String args[])
+    {
+        /**
+         * Prompt the user to enter their favorite integer and then their favorite double.
+         * Use the unconstrained getInt and getDouble methods for this.
+         * Really you only have two tests here, non-numeric input and
+         *      valid numeric input for each method.
+         */
+        Scanner in = new Scanner(System.in);
+        int favInt = SafeInput.getInt(in,"Enter your favorite integer");
+        double favDouble = SafeInput.getDouble(in,"Enter your favorite double");
+        System.out.println("Your favorite integer is " + favInt);
+        System.out.println("Your favorite double is " + favDouble);
+    }
 }
